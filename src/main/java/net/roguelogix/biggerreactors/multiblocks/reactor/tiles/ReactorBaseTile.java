dc2d443a788631f2bab77d2208ed21a9c5b20f91
RogueLogix <rogue@logix.dev>
2024-11-09 13:10:43 -0800

    Update submodules and utilize optimizations made in phos

--- src/main/java/net/roguelogix/biggerreactors/multiblocks/reactor/tiles/ReactorBaseTile.java
+++ src/main/java/net/roguelogix/biggerreactors/multiblocks/reactor/tiles/ReactorBaseTile.java
@@ -6,7 +6,9 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.roguelogix.biggerreactors.multiblocks.reactor.ReactorMultiblockController;
 import net.roguelogix.biggerreactors.multiblocks.reactor.blocks.ReactorBaseBlock;
+import net.roguelogix.phosphophyllite.modular.api.TileModule;
 import net.roguelogix.phosphophyllite.modular.tile.PhosphophylliteTile;
+import net.roguelogix.phosphophyllite.multiblock.MultiblockTileModule;
 import net.roguelogix.phosphophyllite.multiblock.rectangular.IRectangularMultiblockTile;
 import net.roguelogix.phosphophyllite.multiblock.IMultiblockTile;
 import net.roguelogix.phosphophyllite.multiblock.common.IPersistentMultiblockTile;
@@ -44,5 +46,22 @@ public class ReactorBaseTile extends PhosphophylliteTile implements IMultiblockT
     
     public boolean isCurrentController(@Nullable ReactorMultiblockController reactorMultiblockController) {
         return controller() == reactorMultiblockController;
+    }
+    
+    @org.jetbrains.annotations.Nullable
+    private TileModule<?> multiblockTileModule;
+    
+    @Override
+    public MultiblockTileModule<ReactorBaseTile, ReactorBaseBlock, ReactorMultiblockController> multiblockModule() {
+        if(multiblockTileModule == null){
+            multiblockTileModule = module(IMultiblockTile.class);
+        }
+        //noinspection unchecked
+        return (MultiblockTileModule<ReactorBaseTile, ReactorBaseBlock, ReactorMultiblockController>) multiblockTileModule;
+    }
+    
+    @Override
+    public boolean alwaysConnectToSameController() {
+        return true;
     }
 }
